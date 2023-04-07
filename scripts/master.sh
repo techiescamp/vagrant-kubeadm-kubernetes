ed8973ab907646b287d817c52a7111e99422ffe8
Xingjian Zhang <xingjian.zhang@qq.com>
2023-04-07 17:00:37 +0000

    Remove -i in cp for vagrant re-runs

--- scripts/master.sh
+++ scripts/master.sh
@@ -22,7 +22,7 @@ sudo kubeadm init --apiserver-advertise-address=$CONTROL_IP \
   --ignore-preflight-errors Swap $IMAGE_REPOSITORY_FLAG
 
 mkdir -p "$HOME"/.kube
-sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+sudo cp /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 # Save Configs to shared /Vagrant location
@@ -37,7 +37,7 @@ else
   mkdir -p $config_path
 fi
 
-cp -i /etc/kubernetes/admin.conf $config_path/config
+cp /etc/kubernetes/admin.conf $config_path/config
 touch $config_path/join.sh
 chmod +x $config_path/join.sh
 
@@ -52,7 +52,7 @@ kubectl apply -f calico.yaml
 sudo -i -u vagrant bash << EOF
 whoami
 mkdir -p /home/vagrant/.kube
-sudo cp -i $config_path/config /home/vagrant/.kube/
+sudo cp $config_path/config /home/vagrant/.kube/
 sudo chown 1000:1000 /home/vagrant/.kube/config
 EOF
 
