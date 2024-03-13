799ed46534fb26870f87a7eb22dd030b878601e5
Simone Bruzzese <sbruzzese@sighup.io>
2024-03-13 10:19:06 +0100

    fix: add -p to mkdir command

--- scripts/common.sh
+++ scripts/common.sh
@@ -68,7 +68,7 @@ echo "CRI runtime installed successfully"
 
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo mkdir /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
