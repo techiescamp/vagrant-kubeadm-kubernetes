3b1854e8883abd4bcd5e6917956c0b2f506f7450
Pawel Niewiadomski <11110000b@gmail.com>
2024-09-18 10:42:08 +0200

    Fixed failing jq

--- scripts/common.sh
+++ scripts/common.sh
@@ -66,10 +66,30 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/Rele
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 
-sudo apt-get update -y
-sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
-sudo apt-get update -y
-sudo apt-get install -y jq
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |
+      tee /etc/apt/sources.list.d/docker.list
+
+  # Update and install containerd
+  apt-get update -y
+  apt-get install -y containerd.io
+
+  mkdir -p /etc/containerd
+  containerd config default > /etc/containerd/config.toml
+
+  systemctl daemon-reload
+  systemctl enable containerd --now
+  systemctl start containerd
+
+  apt-mark hold containerd.io
+
+  echo "containerd runtime installed successfully"
+fi
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+
+
 apt-get update -y
 apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
 apt-get update -y
@@ -78,8 +98,8 @@ apt-get install -y jq
 # Disable auto-update services
 apt-mark hold kubelet kubectl kubeadm
 
+local_ip="$(ip --json a s | jq -r '.[] | select(.ifname == "eth1") | .addr_info[] | select(.family == "inet") | .local')"
 
-local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "eth1" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')"
 cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
 ${ENVIRONMENT}
